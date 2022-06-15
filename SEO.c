4cabcbde963a1534d3e53ebf89c470b6b4646f5d
Lucas Slechticius <slechticiuslucas@gmail.com>
2022-06-15 17:43:26 -0300

    att 3.0 Atualizando erros de espaçamento entre os textos

--- SEO.c
+++ SEO.c
@@ -12,11 +12,11 @@ int main (void)
     printf("\t Insira a Palavra Chave: \n \n");
     gets(palavraChave);
 
-        printf("\tAqui est%c sua Meta-Description e Meta-Title\n\n ", 160);
+        printf("\n\nAqui est%c sua Meta-Description e Meta-Title\n\n ", 160);
 
             printf("\t%s - Pre%co %s - Comprar %s \n\n", palavraChave, 135, palavraChave, palavraChave);
             printf("\t%s - A FG7 Bebidas %c a mais completa loja de Bebidas. Aqui voc%c encontra as melhores marcas de Whisky, Vodkas e Vinho. Pague parcelado. Entregamos em todo o Brasil. Confira nossas promo%c%ces.\n\n", palavraChave, 130, 136, 135, 228);
-            printf("\t%s, compre %s, pre%co %s, comprar %s, onde comprar %s, loja virtual %s, loja virtual de bebidas, comprar bebidas, bebidas online\n\n", palavraChave, palavraChave, 228, palavraChave, palavraChave, palavraChave, palavraChave);
+            printf("\t%s, compre %s, pre%co %s, comprar %s, onde comprar %s, loja virtual %s, loja virtual de bebidas, comprar bebidas, bebidas online \n\n", palavraChave, palavraChave, 228, palavraChave, palavraChave, palavraChave, palavraChave);
 
 
     }
